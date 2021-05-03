7475760f3a7a779e0bce0401b04349910ea0b586
Isabella <ib2573@cs.ship.edu>
2021-05-03 01:19:44 -0400

    Commented and optimized primefact.c

--- primefact.c
+++ primefact.c
@@ -3,131 +3,145 @@
  * @author Isabella Boone 
  * @author John Gable
  * @author Joshua Lewis
- * @brief Find prime factors of a number. 
- * @version 0.1
+ * @brief Find prime factors of a number using the 
+ *   Pollard Rho prime factorization algorithm. 
+ * @version 0.2
  * @date 2021-04-29
  * 
  * @copyright Copyright (c) 2021
  * 
  */
 
+// Import 
 #include "primefact.h"
 
-void modular_power_mpz(mpz_t var, mpz_t n, mpz_t c) {  
+/**
+ * @brief Calculate (base^exponent)%modulus.  According to geeksforgeeks,
+ *   this is the tortise and hare move. 
+ * 
+ * @param var result to store result in.
+ * @param n mpz_t public key 
+ * @param c mpz_t randomly generated but used as a constant. 
+ */
+void modular_power_mpz(mpz_t result, mpz_t n, mpz_t c) {  
+  // Create 2 in mpz_t. Maybe an easier way, but I don't know it. 
   mpz_t TWO; 
   mpz_init(TWO); 
   mpz_set_ui(TWO, 2); 
 
-  mpz_powm(var, var, TWO, n); // modular_pow(x, 2, n)
-  mpz_add(var, var, c); // add c
-  mpz_add(var, var, n); // add n
-  mpz_mod(var, var, n); // var % n
+  // Calculate modular power according to 
+  mpz_powm(result, result, TWO, n); // modular_pow(x, 2, n)
+  mpz_add(result, result, c); // add c
+  mpz_add(result, result, n); // add n
+  mpz_mod(result, result, n); // var % n
 }
 
+/**
+ * @brief Find prime factors of a number.
+ * 
+ * @param n mpz_t number to find primes of.
+ * @param thread_struct rsa_decrypt_t struct containing information 
+ *   necessary to calculate prime and return information. 
+ */
 void pollardRho(mpz_t n, rsa_decrypt_t *thread_struct) { 
-  gmp_printf("n: %Zd\n", n);
+  // Create constant for 1 and 2 used in calculations 
   mpz_t TWO, ONE; 
   mpz_init(ONE);
   mpz_init(TWO); 
-
-  mpz_set_ui(TWO, 2); 
   mpz_set_ui(ONE, 1); 
+  mpz_set_ui(TWO, 2); 
+
   // Return if has already been found
   if(*(thread_struct->found) == 1) { 
     return;
   }
 
-  // Generate a random number
-
-  // gmp_randinit_mt (gmp_randstate_t state) 
-  // Random state means an algorithm selection and current state data.
+  // Need to initialize a randstate for mpz_urandomb
   gmp_randstate_t state; 
-  gmp_randinit_mt(state);
+  gmp_randinit_mt(state); 
 
-  mpz_t rand_s;
+  // Create & Initialize random number 
+  mpz_t rand_s; 
   mpz_init(rand_s); 
     
   // Generate a uniformly distributed random integer in the range 
-  // 0 to 2^(mpbitcnt) - 1 inclusively. 
+  // 0 to 2^(mpbitcnt) - 1 inclusively, 128 is bitcnt
   mpz_urandomb(rand_s, state, 128);
 
-  if(!mpz_cmp_ui(n, 1)) { // No prime divisor for 1
-    *(thread_struct->found) = 1; 
-    mpz_set(thread_struct->p, n); 
-    return; 
+  // If n == 1, there is no prime divisor for 1.
+  if(!mpz_cmp_ui(n, 1)) { 
+    *(thread_struct->found) = 1; // Set flag for found
+    mpz_set(thread_struct->p, n); // Set p 
+    return; // Return, we've found our divisor
   }  
 
-  // Even means one of the divisors is 2
-  // maybe don't need since n is always odd
+  // Create mpz_t to store result of n % 2
   mpz_t result_mod_2;
   mpz_init(result_mod_2);
-
+  
+  // Calculate n % 2
   mpz_mod_ui(result_mod_2, n, 2); 
+
+  // If n % 2 == 0, n is even, we've found our divisor
   if (!mpz_cmp_ui(result_mod_2, 0)) { 
-    mpz_set_ui(thread_struct->p, 2); 
-    *(thread_struct->found) = 1; 
-    return; 
+    *(thread_struct->found) = 1; // Set flag for found 
+    mpz_set_ui(thread_struct->p, 2); // Set p
+    return; // Return, we've found our divisor 
   }
 
-  // Create & initialize variables to store random ints for finding x, y etc
+  // Create & initialize variables to store randomly generated numbers
   mpz_t rand1, rand2; 
   mpz_init(rand1);
   mpz_init(rand2); 
 
-  // Generate random ints
+  // Generate random numbers
   mpz_urandomb(rand1, state, 128); 
   mpz_urandomb(rand2, state, 128);
 
+  // Create and initialize variables to perform calculations
   mpz_t x, y, c, d, n_copy; 
   mpz_init(x); 
   mpz_init(y); 
-  mpz_init(d); 
+  mpz_init(d); // Just a variable name, albiet confusing 
   mpz_init(c); 
   mpz_init(n_copy); 
 
-  mpz_set(n_copy, n); // Create a copy of n
+  mpz_set(n_copy, n); // Create a copy of n so we can modify it for both calculations
 
-  // maybe should be 3
-  // Calculate x -- uint64_t x = (uint64_t)(rand() % (n - 2)) + 2; 
+  // Calculate x, x picks from range [2, n)
   mpz_sub(n_copy, n, TWO); // (n - 2)
   mpz_mod(x, rand1, n_copy); // rand % n - 2
   mpz_add(x, x, TWO); // (rand % n - 2) + 2
 
-  // Calculate y -- uint64_t y = x; 
+  // Calculate y, which is a copy of x
   mpz_set(y, x);
 
-  // Caclulate c -- uint64_t c = (uint64_t)rand() % (n - 1) + 1;
+  // Caclulate c 
   mpz_sub(n_copy, n, ONE); // n - 1
   mpz_mod(c, rand2, n_copy); // rand % n - 1
   mpz_add(c, c, ONE); // rand % n - 1 + 1
 
-  mpz_set(d, ONE); // uint64_t d = 1l; me thinks? 
+  mpz_set(d, ONE); // Set d = 1
 
+  // While d == 1
   while (!mpz_cmp_ui(d, 1)) { 
-    modular_power_mpz(x, n, c); 
+    // "Tortise move"
+    modular_power_mpz(x, n, c);
+    // "Hare move"
     modular_power_mpz(y, n, c); 
     modular_power_mpz(y, n, c); 
 
-    // gcd(d, abs(x - y), n); 
     mpz_t abs;
-    mpz_sub(abs, x, y); 
-    mpz_abs(abs, abs); 
-    mpz_gcd(d, abs, n);
+    mpz_sub(abs, x, y); // x - y
+    mpz_abs(abs, abs); // abs(x-y)
+    mpz_gcd(d, abs, n); //gcd(abs(x-y), n)
 
+    // If gcd(x-y,n) == n, call again
     if(!mpz_cmp(d, n)) {
       return pollardRho(n, thread_struct);
     }
   }
-  *(thread_struct->found) = 1; 
-  mpz_set(thread_struct->p, d);
+  // d != 1, we found our p value
+  *(thread_struct->found) = 1; // Set found flag
+  mpz_set(thread_struct->p, d); // Set p
 }
-
-
-// int main(void) {
-//   uint64_t n; 
-//   printf("Enter n: \n");
-//   scanf("%ld", &n);
-//   printf("%lu\n", n);
-
-//   printf("One of the divisors for %lu is %lu\n", n, pollardRho(n));
-// }
